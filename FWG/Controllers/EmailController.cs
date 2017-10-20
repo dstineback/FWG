83b20e0e4a0a7d3646c489fc54d96d0533bcac68
Danny Stineback <dstineback@gmail.com>
2017-10-20 09:06:49 -0700

    updated the email page

--- FWG/Controllers/EmailController.cs
+++ FWG/Controllers/EmailController.cs
@@ -35,35 +35,43 @@ namespace FWG.Controllers
         }
 
         [HttpPost]
-        public ActionResult Login(string emailTo, string emailFrom, string emailSubject, string emailBody)
+        public ActionResult Login(string from, string emailFrom, string emailSubject, string emailBody)
         {
-            string s1 = emailFrom;
-            string s2 = emailTo.ToString();
-            string s3 = emailSubject;
-            string s4 = emailBody;
-            sendEmail();
+            try
+            {
 
-            MailMessage mm = new MailMessage();
-            SmtpClient smtp = new SmtpClient();
-            
-            
-            mm.To.Add("developerfwg@gmail.com");
-            mm.To.Add(s1);
-            
-           
-            mm.From = new MailAddress("developerfwg@gmail.com");
-            mm.Body = s1 + Environment.NewLine + s4;
-            mm.Subject = s3;
-            mm.IsBodyHtml = true;
-            smtp.Host = ("smtp.gmail.com");
-            smtp.Port = 587;
-            smtp.Credentials = new NetworkCredential("developerfwg@gmail.com", "FWGinc3411");
-            smtp.EnableSsl = true;
-            
-            smtp.Send(mm);
-            
-            
-            return RedirectToAction("Index", "Home");
+                string s1 = emailFrom;
+                string s2 = from;
+                string s3 = emailSubject;
+                string s4 = emailBody;
+
+                
+
+                MailMessage mm = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+
+
+                mm.To.Add("developerfwg@gmail.com");
+                mm.To.Add(s1);
+
+
+                mm.From = new MailAddress("developerfwg@gmail.com");
+                mm.Body = s2 + Environment.NewLine + s1 + Environment.NewLine + s4;
+                mm.Subject = s3;
+                mm.IsBodyHtml = true;
+                smtp.Host = ("smtp.gmail.com");
+                smtp.Port = 587;
+                smtp.Credentials = new NetworkCredential("developerfwg@gmail.com", "FWGinc3411");
+                smtp.EnableSsl = true;
+
+                smtp.Send(mm);
+
+
+                return RedirectToAction("Index", "Home");
+            } catch
+            {
+                return RedirectToAction("Email", "Email");
+            }
         }
     }
 }
